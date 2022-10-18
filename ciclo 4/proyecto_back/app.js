71c95aae7e1ff7d98ceb6189142dfa29b8134d9d
Jhon Arlem Andrade <jhondoncan@outlook.com>
2022-10-18 15:57:02 -0500

    Ahora es posible registrar nuevos empleados

--- ciclo 4/proyecto_back/app.js
+++ ciclo 4/proyecto_back/app.js
@@ -7,6 +7,7 @@ var database = require('./config/database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var empleadosRouter = require('./routes/empleados.router');
 
 var app = express();
 
@@ -23,6 +24,7 @@ database.mongoConnect();
 // ROUTER
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/empleados', empleadosRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
