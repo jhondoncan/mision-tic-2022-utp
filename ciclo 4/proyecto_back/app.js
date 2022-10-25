ce337d2641eda04a0d99d813d63dea987c93992c
Jhon Arlem Andrade <jhondoncan@outlook.com>
2022-10-25 16:33:50 -0500

    🔒 Se agrega seguridad a las rutas con jwt - se crea usuarios model, controller, router

--- ciclo 4/proyecto_back/app.js
+++ ciclo 4/proyecto_back/app.js
@@ -4,11 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var database = require('./config/database');
+var auth = require('./auth/main_auth')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var empleadosRouter = require('./routes/empleados.router');
 var noviosRouter = require('./routes/novios.router');
+var usuariosRouter = require('./routes/usuario.router');
 
 var app = express();
 
@@ -21,10 +21,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // MONGO CONECTION 
 database.mongoConnect();
+app.use('usuarios', usuariosRouter);
+app.use(auth);
 
 // ROUTER
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/empleados', empleadosRouter);
 app.use('/novios', noviosRouter);
 
